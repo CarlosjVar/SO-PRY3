f5ebedc05d0af6add96a30c817aadb3ef1d9df49
CarlosV <carosjvare@gmai.com>
2021-11-25 10:16:14 -0600

    Returns max drive size on login and signup

--- src/models/user.py
+++ src/models/user.py
@@ -2,12 +2,14 @@ import json
 
 
 class User:
-    def __init__(self, username, password, virtual_dir) -> None:
+    def __init__(self, username, password, virtual_dir, size=0) -> None:
         self.username = username
         self.password = password
         self.virtual_root_directory = virtual_dir
+        self.max_drive_size = size
 
     def toJson(self):
         return json.dumps(self, default=lambda o: o.__dict__)
+
     def __str__(self):
         return f"Username {self.username} password {self.password} dir {self.virtual_root_directory}"
